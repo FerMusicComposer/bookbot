114b05aa8c82e0db36481a2b3d94d3a36907692e
FerMusicComposer <lfernandosalamanca@gmail.com>
2024-07-08 20:06:00 +0200

    added word_count, count_characters and print_report functions

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 def main():
     book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    print(text)
-    print("")
-    words = word_count(text)
-    print(f"the word count for this text is: {words}")
+    print_report(book_path)
 
 
 def word_count(text):
@@ -14,5 +10,28 @@ def word_count(text):
 def get_book_text(path):
     with open("books/frankenstein.txt") as f:
         return f.read()
+    
+def count_characters(text):
+    char_counts = {}
+    for char in text.lower():
+        if char.isalpha():
+            if char in char_counts:
+                char_counts[char] += 1
+            else:
+                char_counts[char] = 1
+    return char_counts
+
+def print_report(book_path):
+    text = get_book_text(book_path)
+    word_count_result = word_count(text)
+    char_counts = count_characters(text)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count_result} words found in the document\n")
+
+    for char, count in sorted(char_counts.items(), key=lambda x: x[1], reverse=True):
+        print(f"The '{char}' character was found {count} times")
+
+    print("--- End report ---")
 
 main()
